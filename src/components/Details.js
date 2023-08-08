cc9e919e1c4c6022d33854561061607fb8929038
William Johnson <catfishfishcat10101@gmail.com>
2023-08-08 04:54:54 -0500

    updated App/js

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,37 +3,61 @@ import { BASE_URL, API_KEY } from '../constants'
 import axios from 'axios'
 
 export default function Details(props) {
-  const { friendId, close } = props
-  const [details, setDetails] = useState(null)
+ const { friendId, close } = props
+ const [details, setDetails] = useState(null)
+ const [currentFriendId, setCurrentFriendId] = useState(friendId)
 
-  // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
+ // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
+ useEffect(() => {
+   axios
+     .get(`${BASE_URL}/friends/${currentFriendId}?api_key=${API_KEY}`)
+     .then((response) => setDetails(response.data))
+     .catch((error) => console.error('Error fetching details:', error));
+ }, [currentFriendId]);
 
-  // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
-  // and puts a 'click' event handler on document.
-  // See what happens if we don't clean up.
+ // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
+ // and puts a 'click' event handler on document.
+ // See what happens if we don't clean up.
+ useEffect(() => {
+   document.addEventListener('click', () => {
+     axios
+       .get(`${BASE_URL}/friends/${currentFriendId}?api_key=${API_KEY}`)
+       .then((response) => setDetails(response.data))
+       .catch((error) => console.error('Error fetching details:', error));
+   });
 
-  // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
+   return () => {
+     document.removeEventListener('click', () => {
+       // Do nothing, as we don't want to clean up the event listener on unmount
+     });
+   };
+ }, [currentFriendId]);
 
-  // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
-  // Whenever props.friendId updates we should trigger a fetch for details of the friend.
-  // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
-  // On success, shove the details of the friend in `details` slice of state
+ // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
+ useEffect(() => {
+   axios
+     .get(`${BASE_URL}/friends/${currentFriendId}?api_key=${API_KEY}`)
+     .then((response) => setDetails(response.data))
+     .catch((error) => console.error('Error fetching details:', error));
+ }, [currentFriendId]);
 
-  return (
-    <div className='container'>
-      <h2>Details (of friend with id {friendId}):</h2>
-      {
-        details &&
-        <>
-          <p>{details.name} is {details.age}</p>
-          <p>email is {details.email}</p>
-          {name} likes:
-          <ul>
-            {details.hobbies.map((hobby) => <li key={hobby}>{hobby}</li>)}
-          </ul>
-        </>
-      }
-      <button onClick={close}>Close</button>
-    </div>
-  )
+ // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
+ // Whenever props.friendId updates we should trigger a fetch for details of the friend.
+ useEffect(() => {
+   setCurrentFriendId(props.friendId);
+ }, [props.friendId]);
+
+ return (
+   <div className='container'>
+     <h2>Details (of friend with id {currentFriendId}):</h2>
+     {details &&
+       <>
+         <p>{details.name} is {details.age}</p>
+         <p>email is {details.email}</p>
+         {details.hobbies.map((hobby) => <li key={hobby}>{hobby}</li>)}
+       </>
+     }
+     <button onClick={close}>Close</button>
+   </div>
+ )
 }
